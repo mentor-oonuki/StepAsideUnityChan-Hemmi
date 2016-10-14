cd7e9221fe47336f79e3576e818fd59bdf5448cf
mentor-oonuki <mentor.oonuki@gmail.com>
2016-10-14 23:54:23 +0900

    【課題回答】

---
+++ Assets/ItemDestroy.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemDestroy : MonoBehaviour {
+
+    private GameObject Unitychan;
+
+	// Use this for initialization
+	void Start () {
+        Unitychan = GameObject.Find("unitychan");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float unitychanZ = Unitychan.transform.position.z;
+        float ItemZ = gameObject.transform.position.z;
+
+        if(unitychanZ - ItemZ > 5)
+        {
+            Destroy(gameObject);
+        }
+
+	}
+}
